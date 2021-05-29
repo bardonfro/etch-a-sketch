d9d8f183e10eaf152df9e27fac64b18214f64f7e
bardonfro <59987699+bardonfro@users.noreply.github.com>
2021-05-28 22:43:14 -0600

    Minor formatting

--- script-blocks.js
+++ script-blocks.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.container');
 
-let cellCount = 30;
-let cellBorderWidth = 1; //Values below 1 crash the layout
+let cellCount = 10;
+let cellBorderWidth = .5; //Values below 1 crash the layout
 let penColor = "rgb(43, 38, 25)";
 
 function buildGrid(size) {
@@ -40,10 +40,10 @@ function buildCell(resolution) {
     const borderAllowance = cellBorderWidth + cellBorderWidth;
     //set cell dimensions
     let portion = 100 / resolution; 
-    width = `calc(${portion}% - ${borderAllowance}px)`;
-    cell.style.width = width;
-    cell.style.borderWidth = `${cellBorderWidth}px`; //Override to fix layout problems if CSS border changes
-    cell.style.height = `calc(100% - ${borderAllowance}px)`;
+    const width = `calc(${portion}% - ${borderAllowance}px)`;
+    //cell.style.width = width;
+    //cell.style.borderWidth = `${cellBorderWidth}px`; //Override to fix layout problems if CSS border changes
+    //cell.style.height = `calc(100% - ${borderAllowance}px)`;
     cell.addEventListener('mouseover',fillCell);
     return cell;
 }
@@ -64,3 +64,4 @@ function resizeGrid (size) {
 
 buildGrid(cellCount);
 
+ 
