d4541e985221790890c0f9d600e6d2427cf8d854
bardonfro <59987699+bardonfro@users.noreply.github.com>
2021-05-30 22:29:32 -0600

    Added functionality of color change buttons and erase button.

--- script-blocks.js
+++ script-blocks.js
@@ -1,8 +1,23 @@
 const container = document.querySelector('.container');
+const bezel = document.querySelector('#bezel');
+const btnErase = document.querySelector('#erase-button');
+const sliderResize = document.querySelector('resize-slider');
+const btnsColor = document.querySelectorAll('.color-button');
+const colorPicker = document.querySelector('#color-picker');
+
+btnErase.addEventListener('click', eraseBoard);
+btnsColor.forEach(addListenerColorChange);
+colorPicker.addEventListener('change', function () {customColor = this.value;});
+
 
 let resolution = [40,30];
 let targetContainerSize = 600;
-let penColor = "rgb(43, 38, 25)";
+let penColor = "rgb(30, 30, 30)";
+let customColor = "#118d25"
+
+function addListenerColorChange(btn) { 
+    btn.addEventListener('click', function() {changePenColor(btn);});
+}
 
 function buildGrid(resWide, resHigh) {
     let gridFrame = document.createElement('div');
@@ -39,8 +54,48 @@ function buildCell() {
     return cell;
 }
 
+function changePenColor (btn) {
+    const current = document.querySelector('button.selected');
+    if (current) {current.classList.remove('selected');};
+    btn.classList.add('selected');
+    
+    
+    penColor = btn.dataset.color;
+    console.log(penColor);
+}
+
+function eraseBoard() {
+    btnErase.classList.add('selected');
+    bezel.addEventListener('animationend',function(){
+        btnErase.classList.remove('selected');
+        bezel.classList.remove('animated');
+    });
+    bezel.classList.add('animated');
+    resizeGrid(resolution[0],resolution[1]);
+}
+
 function fillCell(e) {
-    e.target.style.backgroundColor = penColor;
+    let col
+    switch (penColor) {
+        case "rainbow":
+            col = randomColor();
+            break;
+        case "gradient":
+            col = "rgb(30, 30, 30, 0.5)";
+            break;
+        case "custom":
+            col = customColor;
+            break;
+        default:
+            col = "rgb(30, 30, 30)";
+    }
+
+    e.target.style.backgroundColor = col;
+}
+
+function getSizeFactor (resWide) {
+    const targetCellSize = targetContainerSize / resWide / 10;
+    return targetCellSize + "em"    
 }
 
 function removeGrid() {
@@ -53,20 +108,27 @@ function resizeGrid (resWide, resHigh) {
     buildGrid(resWide, resHigh);
 }
 
-function getSizeFactor (resWide) {
-    const targetCellSize = targetContainerSize / resWide / 10;
-    return targetCellSize + "em"    
+function randomColor() {
+    let R = Math.floor(Math.random()*256);
+    let G = Math.floor(Math.random()*256);
+    let B = Math.floor(Math.random()*256);
+    return `rgb(${R}, ${G}, ${B})`
 }
 
+function setCustomColor (e) {
+    customColor = e.target.value;
+}
+
+//Initial layout on page load
 buildGrid(resolution[0], resolution[1]);
 
+//Function for testing layout
 function testGridSize (minRes, interval, maxRes) {
     let res = minRes;
     let i = 1;
     while (res <= maxRes) {
         resizeGrid(res,res);
         console.log(i + ": " + res + " - " + container.offsetWidth);
-
         ++i
         res = res + interval;
     }
